c69a01809842729799ba8c5f6622e5b63748ab27
Manolache Laurentiu Cristian <mlcristian@tiscali.it>
2019-06-06 02:47:01 +0200

    JS DELETE MILENSTONE !!

--- main.js
+++ main.js
@@ -3,50 +3,66 @@ $(document).ready(function() {
   var url_base = 'http://157.230.17.132:3000/todos/';
   // Color
   var red = {'color': '#e20000'};
-
-// Richiam ajax per prendere l informazioni dal url con il GET
+  var thumb = '<i class="fas fa-thumbtack"></i>';
+  var bin = '<i class="fas fa-trash-alt"></i>';
+  // Richiam ajax per prendere l informazioni dal url con il GET
   print_todo ();
-  post_todo ();
-
-  function print_todo(get) {
-    $.ajax({
-      'url': url_base,
-      'method': 'GET',
-      'success': function(data) {
-        for (var i = 0; i < data.length; i++) {
-          var listcontent = data[i];
-          var thumb = '<i class="fas fa-thumbtack"></i>';
-          $(thumb).css(red);
-          var bin = '<i class="fas fa-trash-alt"></i>';
-          $('#lista').append('<li>' + thumb + listcontent.text + bin +'</li>');
+  add_todo ();
+  task_delete();
+  // AJAX GET
+    function print_todo(get) {
+      $('#lista').html('');
+      $.ajax({
+        'url': url_base,
+        'method': 'GET',
+        'success': function(data) {
+          for (var i = 0; i < data.length; i++) {
+            var listcontent = data[i];
+            $('#lista').append('<li><span id="' + listcontent.id '> + bin</span> ' + listcontent.text + '</li>'  );
+          }
+        },
+        'error': function() {
+          alert('Qualcosa e andato storto !!!');
         }
-      },
-      'error': function() {
-        alert('Qualcosa e andato storto !!!')
-      }
     })
   }
 
   // AJAX post + click
-  function post_todo(post) {
-    $('fa fa-plus-circle').click(function() {
-      var add_text = $('#input').val();
-        $.ajax({
+  function add_todo(post) {
+    $('#circle').click (function () {
+      var add_task = $('#input').val();
+      $('#input').val('');
+      $.ajax ({
+        'url': url_base,
+        'method': 'POST',
+        'data': {
+          'text': add_task
+        },
+        'success': function() {
+          print_todo();
+        },
+        'error': function () {
+          alert('Qualcosa e andato storto !!!');
+        }
+      });
+    });
+  }
+  // Ajax delete
+  function task_delete(cancel) {
+    $('#lista').on('click', 'i' , function() {
+      var id_delete = $(this).attr('data-id');
+      $.ajax({
+        'url': url_base + id_delete,
+        'method': 'DELETE',
+        'success': function() {
+          print_todo();
+        },
+        'error': function() {
+          alert('error');
+        }
+      });
+    });
+  }
 
-          'url': url_base,
-          'method': 'POST',
-          'data': {
-            'text': add_text
-          },
-          'success': function(response) {
-              $('#lista').append('<li>' + thumb + response.text + bin +'</li>');
-            }
-          },
-          'error': function() {
-            alert('Qualcosa e andato storto !!!')
-          }
-        })
-      }
-    })
 
 })
